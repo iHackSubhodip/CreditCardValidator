0d7495bb86675f508565763069fd90221bc51568
Subhodip <differentialthinking@gmail.com>
2019-02-14 15:22:32 +0530

    Proj Structured!

--- CreditCardValidator/CreditCardValidator/Controller/BaseViewController.swift
+++ CreditCardValidator/CreditCardValidator/Controller/BaseViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  BaseViewController.swift
 //  CreditCardValidator
 //
 //  Created by Banerjee,Subhodip on 14/02/19.
@@ -8,13 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class BaseViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-
 }
-
